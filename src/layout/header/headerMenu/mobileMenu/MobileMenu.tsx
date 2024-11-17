e51dfe97e036612ba55b3bedfcceb93b163ce7b7
boykodmitryv <boykodmitryv@mail.ru>
2024-11-17 15:44:35 +0300

    commit

--- src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -1,20 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import {Logo} from "../../../../components/logo/Logo";
 import {Menu} from "../menu/Menu";
 import {ButtonMenu} from "../menu/ButtonMenu";
 import {S} from './../HeaderMenu_Styles'
 
 export const MobileMenu: React.FC = () => {
+    const [menuIsOpen, setmenuIsOpen] = useState(false)
     return (
         <S.MobileMenu aria-modal>
             <Logo/>
             <S.BurgerButton aria-haspopup isOpen={false}>
                 <span></span>
             </S.BurgerButton>
+
             <S.MobileMenuPopup isOpen={false}>
                 <Menu/>
                 <ButtonMenu/>
             </S.MobileMenuPopup>
+
         </S.MobileMenu>
     );
 };
